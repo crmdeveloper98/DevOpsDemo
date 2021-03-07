259d956254931506e74e50f912575bf0154dd7cc
crm developer <crmdeveloper98@gmail.com>
2021-03-07 13:56:30 +0000

    Separated more unit testsSeparated out a unit test for count

--- DevOpsDemo.Tests/PostTestController.cs
+++ DevOpsDemo.Tests/PostTestController.cs
@@ -42,7 +42,39 @@ namespace DevOpsDemo.Tests
         }
 
         [Fact]
-        public void Test_Index_GetPosts_MatchData()
+        public void Test_Index_GetPosts_MatchData_Count()
+        {
+            //Arrange
+            var controller = new HomeController(null,_repository);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<List<PostViewModel>>(viewResult.ViewData.Model);
+
+            Assert.Equal(3, model.Count);
+        }
+
+        [Fact]
+        public void Test_Index_GetPosts_MatchData_Post()
+        {
+            //Arrange
+            var controller = new HomeController(null,_repository);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<List<PostViewModel>>(viewResult.ViewData.Model);
+
+            Assert.Equal(101, model[0].PostId);
+        }
+
+        [Fact]
+        public void Test_Index_GetPosts_MatchData_All()
         {
             //Arrange
             var controller = new HomeController(null,_repository);
